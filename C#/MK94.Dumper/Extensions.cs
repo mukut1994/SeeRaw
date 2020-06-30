d4d19104b5aa80e570dcb51d4c83bd8d5ac542b8
mukut1994 <mukut1994@users.noreply.github.com>
2020-06-30 20:45:50 +0100

    Update Extensions.cs

--- C#/MK94.Dumper/Extensions.cs
+++ C#/MK94.Dumper/Extensions.cs
@@ -3,27 +3,27 @@
 namespace MK94.SirUI
 {
     public static class Extensions
-	{
-		internal static Lazy<Renderer> instance = new Lazy<Renderer>(() => new Renderer());
-
-		public static T Render<T>(this T obj)
-		{
-			return obj.Render(out _);
-		}
-
-		public static T Render<T>(this T obj, out RenderTarget target)
-		{
-			instance.Value.Render(obj, out target);
-
-			return obj;
-		}
-	}
-
-	public static class SirUI
     {
-		public static void OpenDefaultBrowser()
+        internal static Lazy<Renderer> instance = new Lazy<Renderer>(() => new Renderer());
+
+        public static T Render<T>(this T obj)
         {
-			Extensions.instance.Value.OpenBrowser();
+            return obj.Render(out _);
+        }
+
+        public static T Render<T>(this T obj, out RenderTarget target)
+        {
+            instance.Value.Render(obj, out target);
+
+            return obj;
+        }
+    }
+
+    public static class SirUI
+    {
+        public static void OpenDefaultBrowser()
+        {
+            Extensions.instance.Value.OpenBrowser();
         }
     }
 }
