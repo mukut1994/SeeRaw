3a1f28ea2acb583af0566ac9c2b2e824ebbf3ba5
mukut1994 <mukut1994@users.noreply.github.com>
2020-07-29 18:28:44 +0100

    Add progress bar (#14)

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ArrayRenderComponent } from './array-render/array-render.component';
 import { AnyRenderComponent } from './any-render/any-render.component';
 import { ObjectRenderComponent } from './object-render/object-render.component';
 import { FormRenderComponent } from './form-render/form-render.component';
+import { ProgressRenderComponent } from './progress-render/progress-render.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { FormRenderComponent } from './form-render/form-render.component';
     ArrayRenderComponent,
     AnyRenderComponent,
     ObjectRenderComponent,
-    FormRenderComponent
+    FormRenderComponent,
+    ProgressRenderComponent
   ],
   imports: [
     BrowserModule,
