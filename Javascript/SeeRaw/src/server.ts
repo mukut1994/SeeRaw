1b780ecc31af4a3c021212de04dff6f49c42714a
Mithilesh Zavar <mithileshz@hotmail.co.uk>
2021-02-04 17:50:01 +0000

    init

--- Javascript/SeeRaw/src/server.ts
+++ Javascript/SeeRaw/src/server.ts
@@ -2,28 +2,30 @@ import WebSocket from 'websocket'
 import http from 'http'
 import{ RenderTarget } from './RenderTarget';
 
-const port = 3054;
+export class Server {
+  static port = 3054;
 
-let server = http.createServer().listen(port);
+  static WithServer() {
+    let httpServer = http.createServer().listen(this.port);
 
-let wsServer = new WebSocket.server({
-  httpServer: server
-});
+    let wsServer = new WebSocket.server({
+      httpServer: httpServer
+    });
 
-wsServer.on('request', function(request) {
-  let connection = request.accept(null, request.origin);
-
-  
-
-  connection.on('message', (message) => {
-    let firstMessage = new RenderTarget();
-    firstMessage.Value = "Hello World";
-    firstMessage.name = "Hello";
-
-    connection.sendBytes(firstMessage);
-  });
-
-  connection.on('close', function(connection) {
-    // close user connection
-  });
-});
+    wsServer.on('request', function(request) {
+      let connection = request.accept(null, request.origin);
+      
+      connection.on('message', (message) => {
+        let firstMessage = new RenderTarget();
+        firstMessage.Value = "Hello World";
+        firstMessage.name = "Hello";
+    
+        connection.sendBytes(firstMessage);
+      });
+    
+      connection.on('close', function(connection) {
+        // close user connection
+      });
+    });
+  }
+}
